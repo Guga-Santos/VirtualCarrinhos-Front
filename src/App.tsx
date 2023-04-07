62f4e30c1dbf4d1f1c90a166d4052aa250c87d38
GugaSantos <guga.santos.guga@gmail.com>
2023-04-07 17:57:40 -0300

    fix: card

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Card from "./components/card"
+
 function App() {
 
   return (
     <div>
       <h1>Virtual Carrinhos</h1>
+      <Card />
     </div>
   )
 }
---
+++ src/components/card.tsx
@@ -0,0 +1,7 @@
+export default function Card() {
+  return (
+    <div>
+      <h1>Card</h1>
+    </div>
+  )
+}
